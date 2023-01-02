2fd79a6ccd43c3840636958738959f41e11b59c7
Jedrzej Orbik <Jendker@users.noreply.github.com>
2023-01-02 21:36:48 +0100

    Make color loading smoother

--- nvim/lua/plugins.lua
+++ nvim/lua/plugins.lua
@@ -61,6 +61,13 @@ return require('packer').startup(function(use)
         as = 'rose-pine',
         config = function()
           vim.cmd('colorscheme rose-pine')
+          require'rose-pine'.setup({
+            disable_background = true
+          })
+        vim.cmd.colorscheme("rose-pine")
+
+        vim.api.nvim_set_hl(0, "Normal", { bg = "none" })
+        vim.api.nvim_set_hl(0, "NormalFloat", { bg = "none" })
         end,
         cond = { nocode }
       }
