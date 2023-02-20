600ae0a8c10583226bdb2bccd5b4d74544c508c0
Jedrzej Orbik <Jendker@users.noreply.github.com>
2023-02-20 15:49:02 +0100

    Add noice.nvim

--- nvim/lua/plugins.lua
+++ nvim/lua/plugins.lua
@@ -183,8 +183,42 @@ local plugins = {
       {
         "mbbill/undotree",
         config = function()
-          map("n", "<leader>u", vim.cmd.UndotreeToggle)
+          map("n", "<leader>u", "<cmd>UndotreeToggle<CR><C-w>h<CR>")
           vim.opt.undofile = true
+        end,
+        cond = not_vscode
+      },
+      {
+        "folke/noice.nvim",
+        dependencies = {
+          "MunifTanjim/nui.nvim",
+        },
+        config = function()
+          require("noice").setup({
+            lsp = {
+              -- override markdown rendering so that **cmp** and other plugins use **Treesitter**
+              override = {
+                ["vim.lsp.util.convert_input_to_markdown_lines"] = true,
+                ["vim.lsp.util.stylize_markdown"] = true,
+              },
+              signature = {
+                enabled = false
+              }
+            },
+            -- you can enable a preset for easier configuration
+            presets = {
+              bottom_search = true, -- use a classic bottom cmdline for search
+              command_palette = true, -- position the cmdline and popupmenu together
+              long_message_to_split = true, -- long messages will be sent to a split
+            },
+            messages = {
+              enabled = true,
+              view_search = "mini",
+            },
+            notify = {
+              enabled = false
+            }
+          })
         end,
         cond = not_vscode
       }
