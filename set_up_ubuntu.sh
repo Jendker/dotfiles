bb86a3ddc74f07d74f302db8edeced0b6997b61e
Jedrzej Orbik <Jendker@users.noreply.github.com>
2023-04-17 09:59:19 +0200

    Install instead of sync Lazy on bootstrap

--- set_up_ubuntu.sh
+++ set_up_ubuntu.sh
@@ -19,7 +19,7 @@ function update_nvim() {
   cd /tmp/ && rm -rf dotfiles && git clone https://github.com/Jendker/dotfiles.git
   cp -r dotfiles/nvim $HOME/.config
   run_times=1
-  nvim --headless "+Lazy! sync" +qa
+  nvim --headless "+Lazy! install" +qa
   # repeat again until successful
   while [ $? -ne 0 ]; do !!; done
   rm -rf dotfiles
