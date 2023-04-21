ef9b021fea79a9eb2edf9506e942aa0d3c022684
Jedrzej Orbik <Jendker@users.noreply.github.com>
2023-04-21 11:44:33 +0200

    Use only a single column sign for LSP

--- nvim/lua/config_plugins/lsp-zero.lua
+++ nvim/lua/config_plugins/lsp-zero.lua
@@ -307,3 +307,40 @@ local ok, cmp_autopairs = pcall(require, 'nvim-autopairs.completion.cmp')
 if ok then
   cmp.event:on('confirm_done', cmp_autopairs.on_confirm_done())
 end
+
+-- Taken from :help diagnostic-handlers-example
+-- It reduces LSP signs to only more severe one and makes LSP signs coexist nicely with Gitsigns
+
+-- Create a custom namespace. This will aggregate signs from all other
+-- namespaces and only show the one with the highest severity on a
+-- given line
+local ns = vim.api.nvim_create_namespace("my_namespace")
+
+-- Get a reference to the original signs handler
+local orig_signs_handler = vim.diagnostic.handlers.signs
+
+-- Override the built-in signs handler
+vim.diagnostic.handlers.signs = {
+  show = function(_, bufnr, _, opts)
+    -- Get all diagnostics from the whole buffer rather than just the
+    -- diagnostics passed to the handler
+    local diagnostics = vim.diagnostic.get(bufnr)
+
+    -- Find the "worst" diagnostic per line
+    local max_severity_per_line = {}
+    for _, d in pairs(diagnostics) do
+      local m = max_severity_per_line[d.lnum]
+      if not m or d.severity < m.severity then
+        max_severity_per_line[d.lnum] = d
+      end
+    end
+
+    -- Pass the filtered diagnostics (with our custom namespace) to
+    -- the original handler
+    local filtered_diagnostics = vim.tbl_values(max_severity_per_line)
+    orig_signs_handler.show(ns, bufnr, filtered_diagnostics, opts)
+  end,
+  hide = function(_, bufnr)
+    orig_signs_handler.hide(ns, bufnr)
+  end,
+}
