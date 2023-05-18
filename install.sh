3adc1a4a7ea4798fc7af9bcc4d2267a38f754e76
Jedrzej Orbik <Jendker@users.noreply.github.com>
2023-05-18 23:52:00 +0200

    Copy the config on MacOS instead of symlink - I use mackup

--- install.sh
+++ install.sh
@@ -1,4 +1,12 @@
 #!/usr/bin/env bash
 
-rm -rf ~/.config/nvim
-ln -s $(pwd)/nvim ~/.config/nvim
+if [[ "$OSTYPE" == "linux-gnu"* ]]; then
+  rm -rf ~/.config/nvim
+  ln -s $(pwd)/nvim ~/.config/nvim
+elif [[ "$OSTYPE" == "darwin"* ]]; then
+  rm -r "/Users/jedrzej/Library/Mobile Documents/com~apple~CloudDocs/Mackup/.config/nvim/"
+  cp -r $(pwd)/nvim/ "/Users/jedrzej/Library/Mobile Documents/com~apple~CloudDocs/Mackup/.config/nvim/"
+else
+  echo "OS type unknown. Exiting."
+  exit 1
+fi
