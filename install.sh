b7ba2a61c8c85b915eaa65109ba87d64384e2a2b
Jedrzej Orbik <Jendker@users.noreply.github.com>
2023-06-12 17:15:14 +0200

    Add clangd

--- install.sh
+++ install.sh
@@ -1,11 +1,24 @@
 #!/usr/bin/env bash
 
+# nvim
 if [[ "$OSTYPE" == "linux-gnu"* ]]; then
   rm -rf ~/.config/nvim
   ln -s $(pwd)/nvim ~/.config/nvim
 elif [[ "$OSTYPE" == "darwin"* ]]; then
   rm -r "/Users/jedrzej/Library/Mobile Documents/com~apple~CloudDocs/Mackup/.config/nvim/"
   cp -r $(pwd)/nvim/ "/Users/jedrzej/Library/Mobile Documents/com~apple~CloudDocs/Mackup/.config/nvim/"
+else
+  echo "OS type unknown. Exiting."
+  exit 1
+fi
+
+# clangd
+if [[ "$OSTYPE" == "linux-gnu"* ]]; then
+  rm -rf ~/.config/clangd
+  ln -s $(pwd)/clangd ~/.config/clangd
+elif [[ "$OSTYPE" == "darwin"* ]]; then
+  rm -r "/Users/jedrzej/Library/Mobile Documents/com~apple~CloudDocs/Mackup/.config/clangd/"
+  cp -r $(pwd)/clangd/ "/Users/jedrzej/Library/Mobile Documents/com~apple~CloudDocs/Mackup/.config/clangd/"
 else
   echo "OS type unknown. Exiting."
   exit 1
