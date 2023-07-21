188bf5e5524ed6f77ac4e39888b10edce7bfe15d
Jedrzej Orbik <Jendker@users.noreply.github.com>
2023-07-22 00:05:16 +0200

    Add macos script

---
+++ macos/set_defaults.sh
@@ -0,0 +1,41 @@
+# Close any open System Preferences panes, to prevent them from overriding
+# settings we’re about to change
+osascript -e 'tell application "System Preferences" to quit'# Disable press-and-hold for keys in favor of key repeat.
+
+defaults write -g ApplePressAndHoldEnabled -bool false
+
+# Set a really fast key repeat. -- original = 2, another option 1
+defaults write NSGlobalDomain KeyRepeat -int 2
+# original 15, another option 10
+defaults write NSGlobalDomain InitialKeyRepeat -int 15
+
+# Expand save panel by default
+defaults write NSGlobalDomain NSNavPanelExpandedStateForSaveMode -bool true
+defaults write NSGlobalDomain NSNavPanelExpandedStateForSaveMode2 -bool true
+
+# Expand print panel by default
+defaults write NSGlobalDomain PMPrintingExpandedStateForPrint -bool true
+defaults write NSGlobalDomain PMPrintingExpandedStateForPrint2 -bool true
+
+# Disable auto-correct
+defaults write NSGlobalDomain NSAutomaticSpellingCorrectionEnabled -bool false
+
+# Finder: show path bar
+defaults write com.apple.finder ShowPathbar -bool true
+
+# Keep folders on top when sorting by name
+defaults write com.apple.finder _FXSortFoldersFirst -bool true
+
+# When performing a search, search the current folder by default
+defaults write com.apple.finder FXDefaultSearchScope -string "SCcf"
+
+# Avoid creating .DS_Store files on network or USB volumes
+defaults write com.apple.desktopservices DSDontWriteNetworkStores -bool true
+defaults write com.apple.desktopservices DSDontWriteUSBStores -bool true
+
+###############################################################################
+# General UI/UX                                                               #
+###############################################################################
+
+# Disable the sound effects on boot
+sudo nvram StartupMute=%01
