37f3ec0ec3c8d00de0f5dab9f394e759b6130af2
Jedrzej Orbik <Jendker@users.noreply.github.com>
2024-01-09 14:19:21 -0600

    Use autocomplete with cmp-cmdline for :

--- nvim/lua/config_plugins/lsp-zero.lua
+++ nvim/lua/config_plugins/lsp-zero.lua
@@ -460,11 +460,14 @@ cmp.setup.cmdline('/', {
 -- Use cmdline & path source for ':'.
 cmp.setup.cmdline(':', {
   mapping = cmp.mapping.preset.cmdline(),
-  completion = { autocomplete = false },
   sources = cmp.config.sources({
     { name = 'path' }
   }, {
-    { name = 'cmdline' }
+    { name = 'cmdline',
+      option = {
+        ignore_cmds = { 'Man', '!' }
+      },
+    },
   })
 })
 
