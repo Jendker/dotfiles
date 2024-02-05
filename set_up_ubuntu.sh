ad000347e5154de7afe3f0a64dc317da39cfcc33
Jedrzej Orbik <Jendker@users.noreply.github.com>
2024-02-05 02:30:45 -0600

    Install conda only if not available

--- set_up_ubuntu.sh
+++ set_up_ubuntu.sh
@@ -260,7 +260,7 @@ fi
 
 
 # set up miniconda
-if [ ! -d "$HOME/miniconda3" ]; then
+if ! [ -x "$(command -v conda)" ]; then
   mkdir -p ~/miniconda3
   wget https://repo.anaconda.com/miniconda/Miniconda3-latest-Linux-x86_64.sh -O ~/miniconda3/miniconda.sh
   bash ~/miniconda3/miniconda.sh -b -u -p ~/miniconda3
