3f7afe6681f358c7b35bf1957639be51972fcfb1
Jedrzej Orbik <Jendker@users.noreply.github.com>
2024-08-12 13:18:28 +0200

    Bug fix macos install

--- macos/install.sh
+++ macos/install.sh
@@ -4,8 +4,9 @@ set -e
 SCRIPT_DIR=$(dirname $0)
 ROOT_DIR=$(dirname "$SCRIPT_DIR")
 
-ROOT_DIR/set_up_common.sh
+$ROOT_DIR/set_up_common.sh
 
+mkdir -p "$HOME/Library/Keyboard Layouts/"
 cp "/Users/jedrzej/Library/Mobile Documents/com~apple~CloudDocs/Random/Keyboard Layouts/Polish-German.bundle" "$HOME/Library/Keyboard Layouts/"
 echo "Reboot and add the Polish-German keyboard layout from Polish keyboards"
 cp ROOT_DIR/dotfiles_private/fonts/* ~/Library/Fonts/
