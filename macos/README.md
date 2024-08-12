07c255d7ea31c11af040d471aaa9b5c4d0e0392c
Jedrzej Orbik <Jendker@users.noreply.github.com>
2024-08-12 12:41:16 +0200

    Move the ssh config to macos/install.sh

--- macos/README.md
+++
@@ -1,5 +0,0 @@
-In /etc/ssh/sshd_config.d/100-macos.conf add the following lines to disable password authentication:
-```
-PasswordAuthentication no
-ChallengeResponseAuthentication no
-```
--- macos/install.sh
+++ macos/install.sh
@@ -9,3 +9,9 @@ ROOT_DIR/set_up_common.sh
 cp "/Users/jedrzej/Library/Mobile Documents/com~apple~CloudDocs/Random/Keyboard Layouts/Polish-German.bundle" "$HOME/Library/Keyboard Layouts/"
 echo "Reboot and add the Polish-German keyboard layout from Polish keyboards"
 cp ROOT_DIR/dotfiles_private/fonts/* ~/Library/Fonts/
+
+SSH_CONFIG_PATH=/etc/ssh/sshd_config.d/100-macos.conf
+if ! grep -Fxq "PasswordAuthentication no" "$SSH_CONFIG_PATH"; then
+  echo "PasswordAuthentication no" >> "$SSH_CONFIG_PATH"
+  echo "ChallengeResponseAuthentication no" >> "$SSH_CONFIG_PATH"
+fi
