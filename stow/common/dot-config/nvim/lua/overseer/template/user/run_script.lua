8de52fd4547f95bb4fc34c395b41437595b638fb
Jedrzej Orbik <Jendker@users.noreply.github.com>
2024-12-02 00:16:18 +0100

    Update the 'Run script' overseer

--- stow/common/dot-config/nvim/lua/overseer/template/user/run_script.lua
+++ stow/common/dot-config/nvim/lua/overseer/template/user/run_script.lua
@@ -2,9 +2,19 @@ return {
   name = "run script",
   builder = function()
     local file = vim.fn.expand("%:p")
-    local cmd = { file }
-    if vim.bo.filetype == "go" then
-      cmd = { "go", "run", file }
+    local filetype_cmds = {
+      go = { "go", "run" },
+      python = { "python" },
+      sh = { "bash" },
+    }
+
+    local filetype = vim.bo.filetype
+    local cmd = filetype_cmds[filetype]
+
+    if cmd then
+      table.insert(cmd, file)
+    else
+      vim.notify("Oversser template - Unsupported filetype: " .. filetype)
     end
     return {
       cmd = cmd,
