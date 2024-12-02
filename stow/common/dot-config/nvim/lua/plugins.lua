0552234539e57bb0a5e35692875c5059cfd293d1
Jedrzej Orbik <Jendker@users.noreply.github.com>
2024-12-02 11:55:45 +0100

    Don't use CopilotChat canary branch

--- stow/common/dot-config/nvim/lua/plugins.lua
+++ stow/common/dot-config/nvim/lua/plugins.lua
@@ -1223,7 +1223,6 @@ local plugins = {
     "CopilotC-Nvim/CopilotChat.nvim",
     cmd = {"CopilotChat"},
     event = "InsertEnter",
-    branch = "canary",
     dependencies = {
       {"zbirenbaum/copilot.lua"}, -- or github/copilot.vim
       {"nvim-lua/plenary.nvim"}, -- for curl, log wrapper
