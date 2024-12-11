d7b8132d1d9ef8ef64ed8d7ffaa9b05f7cf95808
Jedrzej Orbik <Jendker@users.noreply.github.com>
2024-12-11 19:59:10 +0100

    Auto-pair $ with tex

--- stow/common/dot-config/nvim/lua/plugins.lua
+++ stow/common/dot-config/nvim/lua/plugins.lua
@@ -593,6 +593,17 @@ local plugins = {
     'windwp/nvim-autopairs',
     event = "InsertEnter",
     opts = {},
+    config = function(_, opts)
+      require('nvim-autopairs').setup(opts)
+      local Rule = require('nvim-autopairs.rule')
+      local npairs = require('nvim-autopairs')
+      npairs.add_rule(
+        Rule("$", "$", "tex")
+        :with_move(function(args)
+          return args.next_char == args.char
+        end)
+      )
+    end,
     cond = not_vscode
   },
   {
