ad9047d08c6e2c2ea7773124f0313df4d77cfccd
Jedrzej Orbik <Jendker@users.noreply.github.com>
2025-01-08 12:24:53 +0100

    Fix powerline10k install

--- scripts/debian/install.sh
+++ scripts/debian/install.sh
@@ -28,6 +28,25 @@ function setup_nerdfont() {
 function install_powerlevel10k() {
   git clone --depth=1 https://github.com/romkatv/powerlevel10k.git "${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/themes/powerlevel10k" || true
   sed -i 's/ZSH_THEME=\"robbyrussell\"/ZSH_THEME=\"powerlevel10k\/powerlevel10k\"/g' "$HOME/.zshrc"
+  if ! grep -q "# Enable Powerlevel10k instant prompt. Should stay close to the top of ~/.zshrc." ~/.zshrc; then
+    cat << 'EOF' > temp
+# Enable Powerlevel10k instant prompt. Should stay close to the top of ~/.zshrc.
+# Initialization code that may require console input (password prompts, [y/n]
+# confirmations, etc.) must go above this block; everything else may go below.
+if [[ -r "${XDG_CACHE_HOME:-$HOME/.cache}/p10k-instant-prompt-${(%):-%n}.zsh" ]]; then
+  source "${XDG_CACHE_HOME:-$HOME/.cache}/p10k-instant-prompt-${(%):-%n}.zsh"
+fi
+
+EOF
+    cat temp ~/.zshrc > temp2 && mv temp2 ~/.zshrc
+    rm temp
+
+    cat << 'EOF' >> ~/.zshrc
+
+# To customize prompt, run `p10k configure` or edit ~/.p10k.zsh.
+[[ ! -f ~/.p10k.zsh ]] || source ~/.p10k.zsh
+EOF
+  fi
 }
 
 function install_sublimetext() {
